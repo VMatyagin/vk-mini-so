5609adeffe5f12840a0b62160f0e2ef3a1c8ed33
Vladimir Matyagin <ghost-email@yandex.ru>
2021-02-10 21:52:53 +0300

    * | fixed errors

--- src/feature/VKBridge/index.ts
+++ src/feature/VKBridge/index.ts
@@ -1,6 +1,6 @@
 import VKBridge, {
-  VKBridgeEvent,
-  AnyReceiveMethodName,
+    VKBridgeEvent,
+    AnyReceiveMethodName,
 } from "@vkontakte/vk-bridge";
 import { rootStore } from "../stores";
 
@@ -8,93 +8,91 @@ const APP_ID = 7555628;
 const API_VERSION = "5.122";
 
 export const initApp = () => {
-  const VKConnectCallback = (e: VKBridgeEvent<AnyReceiveMethodName>) => {
-    if (e.detail.type === "VKWebAppUpdateConfig") {
-      VKBridge.unsubscribe(VKConnectCallback);
-      rootStore.app.setColorScheme(e.detail.data.scheme);
-    }
-  };
+    const VKConnectCallback = (e: VKBridgeEvent<AnyReceiveMethodName>) => {
+        if (e.detail.type === "VKWebAppUpdateConfig") {
+            VKBridge.unsubscribe(VKConnectCallback);
+            rootStore.app.setColorScheme(e.detail.data.scheme);
+        }
+    };
 
-  VKBridge.subscribe(VKConnectCallback);
+    VKBridge.subscribe(VKConnectCallback);
 
-  return VKBridge.send("VKWebAppInit", {})
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => {
-      return error;
-    });
+    return VKBridge.send("VKWebAppInit", {})
+        .then((data) => {
+            return data;
+        })
+        .catch((error) => {
+            return error;
+        });
 };
 
 export const getAuthToken = async (scope: any) => {
-  await VKBridge.send("VKWebAppGetAuthToken", {
-    app_id: APP_ID,
-    scope: scope.join(","),
-  })
-    .then((data) => {
-      rootStore.app.setAccessToken(data.access_token);
+    await VKBridge.send("VKWebAppGetAuthToken", {
+        app_id: APP_ID,
+        scope: scope.join(","),
     })
-    .catch(() => {
-      rootStore.app.setAccessToken("");
-    });
+        .then((data) => {
+            rootStore.app.setAccessToken(data.access_token);
+        })
+        .catch(() => {
+            rootStore.app.setAccessToken("");
+        });
 };
 export const getUserData = () => VKBridge.send("VKWebAppGetUserInfo");
 
 export const closeApp = () => {
-  return VKBridge.send("VKWebAppClose", {
-    status: "success",
-  })
-    .then((data) => {
-      return data;
+    return VKBridge.send("VKWebAppClose", {
+        status: "success",
     })
-    .catch((error) => {
-      return error;
-    });
+        .then((data) => {
+            return data;
+        })
+        .catch((error) => {
+            return error;
+        });
 };
 
 export const swipeBackOn = () => {
-  return VKBridge.send("VKWebAppEnableSwipeBack", {})
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => {
-      return error;
-    });
+    return VKBridge.send("VKWebAppEnableSwipeBack", {})
+        .then((data) => {
+            return data;
+        })
+        .catch((error) => {
+            return error;
+        });
 };
 
 export const swipeBackOff = () => {
-  return VKBridge.send("VKWebAppDisableSwipeBack", {})
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => {
-      return error;
-    });
+    return VKBridge.send("VKWebAppDisableSwipeBack", {})
+        .then((data) => {
+            return data;
+        })
+        .catch((error) => {
+            return error;
+        });
 };
 
 export const getWallPosts = () => {
-  return APICall("wall.get", {
-    owner_id: "-12906",
-    extended: "1",
-    count: "10",
-  });
+    return APICall("wall.get", {
+        owner_id: "-12906",
+        extended: "1",
+        count: "10",
+    });
 };
 
-export const APICall = (
-  method: string,
-  params: Record<string, string | number>
-) => {
-  params["access_token"] = rootStore.app.accessToken;
-  params["v"] = params["v"] === undefined ? API_VERSION : params["v"];
-
-  return VKBridge.send("VKWebAppCallAPIMethod", {
-    method: method,
-    params: params,
-  })
-    .then((data) => {
-      return data.response;
+export const APICall = (method: string, params: Record<string, string>) => {
+    return VKBridge.send("VKWebAppCallAPIMethod", {
+        method,
+        params: {
+            ...params,
+            access_token: rootStore.app.accessToken,
+            v: params["v"] === undefined ? API_VERSION : params["v"],
+        },
     })
-    .catch((error) => {
-      return error;
-    });
+        .then((data) => {
+            return data.response;
+        })
+        .catch((error) => {
+            return error;
+        });
 };
