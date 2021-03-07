ba1dc7df26e215baa41ca425077acd33d6046e1c
Vladimir Matyagin <ghost-email@yandex.ru>
2021-03-07 11:49:29 +0300

    * | remove excess panel

--- src/features/event/EventHandlingView.tsx
+++ src/features/event/EventHandlingView.tsx
@@ -4,7 +4,6 @@ import { EventEditPanel } from "./pages/EventEditPanel";
 import { EventHandlePanel } from "./pages/EventHandlePanel";
 import { EventPagePanel } from "./pages/EventPagePanel";
 import { EventUsers } from "./pages/EventUsers";
-import { EventWinners } from "./pages/EventWinners";
 import { SearchPanel } from "./pages/SearchPanel";
 import { WalletPanel } from "./pages/WalletPanel";
 
@@ -22,8 +21,6 @@ export const EventHandlingView: FC<{ id: string }> = ({ id }) => {
             <EventUsers id="event_organizers" type="organizers" />
             <EventUsers id="event_volonteers" type="volonteers" />
             <EventUsers id="event_page_artists" type="artists" />
-
-            <EventWinners id="event_page_winners" />
         </AbstractView>
     );
 };
