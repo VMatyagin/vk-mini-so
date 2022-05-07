9d287a0e9b6fb977a01004afee0b4eb08b19e67b
Vladimir Matyagin <ghost-email@yandex.ru>
2022-05-07 16:35:42 +0300

    * | shtab | after edit behavior changed #19

--- src/features/shtab/ui/molecules/MainInfoForm/index.tsx
+++ src/features/shtab/ui/molecules/MainInfoForm/index.tsx
@@ -1,11 +1,11 @@
 import {
-  Button,
-  FormItem,
-  FormLayout,
-  Group,
-  Header,
-  Input,
-  ScreenSpinner,
+    Button,
+    FormItem,
+    FormLayout,
+    Group,
+    Header,
+    Input,
+    ScreenSpinner
 } from "@vkontakte/vkui";
 import { observer } from "mobx-react-lite";
 import { FC, useContext, useMemo } from "react";
@@ -17,72 +17,78 @@ import { Shtab } from "../../../../types";
 import { ShtabsAPI } from "../../../../utils/requests/shtab-request";
 
 export const MainInfoForm: FC = observer(() => {
-  const { openPopout, closePopout } = useContext(routerStore);
-  const { route, router } = useRoute();
-  const { shtabId } = useMemo(() => route.params, [route]);
+    const { openPopout, closePopout } = useContext(routerStore);
+    const { route, router } = useRoute();
+    const { shtabId } = useMemo(() => route.params, [route]);
 
-  const { data: brigade } = useQuery({
-    queryKey: ["shtab", shtabId!],
-    queryFn: ({ queryKey }) => {
-      openPopout(<ScreenSpinner />);
-      return ShtabsAPI.getShtab(queryKey[1] as number);
-    },
-    retry: 1,
-    refetchOnWindowFocus: false,
-    enabled: !!shtabId,
-    onSuccess: closePopout,
-    onError: closePopout,
-  });
+    const { data: brigade } = useQuery({
+        queryKey: ["shtab", shtabId!],
+        queryFn: ({ queryKey }) => {
+            openPopout(<ScreenSpinner />);
+            return ShtabsAPI.getShtab(queryKey[1] as number);
+        },
+        retry: 1,
+        refetchOnWindowFocus: false,
+        enabled: !!shtabId,
+        onSuccess: closePopout,
+        onError: closePopout
+    });
 
-  const { handleSubmit, control, formState } = useForm<Shtab>({
-    defaultValues: { ...brigade! },
-    mode: "onChange",
-  });
-  const { isDirty, isValid } = formState;
+    const { handleSubmit, control, formState } = useForm<Shtab>({
+        defaultValues: { ...brigade! },
+        mode: "onChange"
+    });
+    const { isDirty, isValid } = formState;
 
-  const onSubmit = async (values: Shtab) => {
-    openPopout(<ScreenSpinner />);
-    await ShtabsAPI.updateShtab(values)
-      .then(() => {
-        router.navigate("else.shtab.details", { shtabId });
-      })
-      .finally(closePopout);
-  };
+    const onSubmit = async (values: Shtab) => {
+        openPopout(<ScreenSpinner />);
+        await ShtabsAPI.updateShtab(values)
+            .then(() => {
+                router.navigate(
+                    "else.shtab.details",
+                    { shtabId },
+                    { replace: true }
+                );
+            })
+            .finally(closePopout);
+    };
 
-  return (
-    <Group header={<Header mode="secondary">Информация о штабе</Header>}>
-      <FormLayout onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          control={control}
-          name="title"
-          rules={{ required: "Это поле необходимо заполнить" }}
-          render={({ field, fieldState }) => (
-            <FormItem
-              top="Название"
-              status={fieldState.invalid ? "error" : "default"}
-              bottom={fieldState.error && fieldState.error.message}
-            >
-              <Input
-                type="text"
-                name={field.name}
-                value={field.value}
-                onChange={field.onChange}
-                onBlur={field.onBlur}
-              />
-            </FormItem>
-          )}
-        />
-        <FormItem>
-          <Button
-            size="l"
-            stretched={true}
-            type="submit"
-            disabled={!isDirty || !isValid}
-          >
-            Сохранить
-          </Button>
-        </FormItem>
-      </FormLayout>
-    </Group>
-  );
+    return (
+        <Group header={<Header mode="secondary">Информация о штабе</Header>}>
+            <FormLayout onSubmit={handleSubmit(onSubmit)}>
+                <Controller
+                    control={control}
+                    name="title"
+                    rules={{ required: "Это поле необходимо заполнить" }}
+                    render={({ field, fieldState }) => (
+                        <FormItem
+                            top="Название"
+                            status={fieldState.invalid ? "error" : "default"}
+                            bottom={
+                                fieldState.error && fieldState.error.message
+                            }
+                        >
+                            <Input
+                                type="text"
+                                name={field.name}
+                                value={field.value}
+                                onChange={field.onChange}
+                                onBlur={field.onBlur}
+                            />
+                        </FormItem>
+                    )}
+                />
+                <FormItem>
+                    <Button
+                        size="l"
+                        stretched={true}
+                        type="submit"
+                        disabled={!isDirty || !isValid}
+                    >
+                        Сохранить
+                    </Button>
+                </FormItem>
+            </FormLayout>
+        </Group>
+    );
 });
